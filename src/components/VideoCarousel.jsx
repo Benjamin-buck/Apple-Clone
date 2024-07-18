b725055166bde7a2b937ca13dd436941389f6bae
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-07-18 09:11:09 -0400

    Adds video 1 play feature

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from 'react'
 import { hightlightsSlides } from '../constants'
 import gsap from 'gsap';
+import { pauseImg, playImg, replayImg } from '../utils';
+import { useGSAP } from '@gsap/react';
 
 const VideoCarousel = () => {
   // video reference variables
@@ -28,11 +30,29 @@ const VideoCarousel = () => {
   // can now use these without typing video._______
   const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video
 
+  // Use GSAP to update animations when the video changes
+  useGSAP(() => {
+    // animate gsap to play when the video is in view
+    gsap.to('#video', {
+        scrollTrigger: {
+            trigger: '#video',
+            toggleActions: 'restart none none none',
+        },
+        onComplete: () => {
+            setVideo((pre) => ({
+                ...pre,
+                startPlay: true,
+                isPlaying: true,
+            }))
+        }
+    })
+  }, [isEnd, videoId])
+
 
   // Deals with the playing of the video
   // can be retriggered when the startPlay, videoId, isPlaying, loadedData changes.
   useEffect(() => {
-    if(loadedData.length < 3) {
+    if(loadedData.length > 3) {
         if(!isPlaying) {
             videoRef.current[videoId].pause();
         } else {
@@ -40,6 +60,12 @@ const VideoCarousel = () => {
         }
     }
   }, [startPlay, videoId, isPlaying, loadedData])
+
+
+  //This function will accept the index / event passed into it
+  // call the setLoadedData function. Take all previously loaded data and automatically return an array with the new event
+
+  const handleLoadedMetaData = (i, e) => setLoadedData((pre) => [...pre, e]); 
   
 
 
@@ -72,6 +98,41 @@ const VideoCarousel = () => {
     }
   }, [videoId, startPlay])
 
+  // a function to define the handling of the onClick
+  // use a switch whenever there are multiple cases.
+  const handleProcess = (type, i) => {
+    switch (type) {
+        // Case 1:
+        // if the case is videoEnd, set the video isEnd state to true
+        // increment the video ID + 1
+        case 'video-end':
+            setVideo((pre) => ({...pre, isEnd: true, videoId: i + 1}))
+            break;
+    
+        // Case 2:
+        // if on the last video, but not yet the end.
+        // spread the previous video and set the isLastVideo to true
+        case 'video-last':
+            setVideo((pre) => ({ ...pre, isLastVideo: true}))
+            break;
+
+        // Case 3:
+        // Duplicate set video & isLastVideo = false, reset the video ID to 0.
+        case 'video-reset':
+            setVideo((pre) => ({...pre, isLastVideo: false, videoId: 0}))
+            break;
+        
+        // Case 4: Play
+        // Set is playing to the opposite of previous.isplaying
+        case 'play':
+            setVideo((pre) => ({...pre, isPlaying: !pre.isPlaying}))
+            break;
+
+        default:
+            return video;
+    }
+  }
+
 
   return (
     <>
@@ -88,11 +149,14 @@ const VideoCarousel = () => {
                               muted
                               // finding a specific index in the videoRefs array and setting to the current video element
                               ref={(el) => (videoRef.current[i] = el)}
+
+                              // spread all the information about the video, but set the is palying to true.   
                               onPlay={() => {
                                 setVideo((prevVideo) => ({
                                     ...prevVideo, isPlaying: true
                                 }))
                               }}
+                              onLoadedMetadata={(e) => handleLoadedMetaData(i, e)}
                             >
                                 <source src={list.video} type='video/mp4'/>
                             </video>
@@ -105,6 +169,34 @@ const VideoCarousel = () => {
                     </div>
                 </div>
             ))}
+        </div>
+        <div className="relative flex-center mt-10">
+            <div className='flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full'>
+                {/* get each video, and then get the index. For each video ref, return a span element
+                that span element will have a key and a ref. */}
+                {videoRef.current.map((_, i) => (
+                    <span
+                        key={i}
+                        ref={(el) => (videoDivRef.current[i] = el)} 
+                        className='mx-2 w-3 h-3 bg-gray-200 rounded-full relative cursor-pointer'
+                    >
+                        <span className='absolute h-full w-full rounded-full' ref={(el) => (videoSpanRef.current[i] = el)} />
+                
+                    </span>
+                ))}
+            </div>
+            <button className='control-btn'>
+                <img 
+                src={isLastVideo ? replayImg : !isPlaying ? playImg : pauseImg} 
+                alt={isLastVideo ? 'replay' : !isPlaying ? 'play' : 'pause'} 
+                onClick={isLastVideo
+                    ? () => handleProcess('video-reset')
+                    : !isPlaying
+                        ? () => handleProcess('play')
+                        : () => handleProcess('pause')
+                }
+                />
+            </button>
         </div>
     </>
   )
